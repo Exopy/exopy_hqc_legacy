76f6ccf0ee7a45628b91af42fea9ddef2f09f379
leghtas <leghtas@gmail.com>
2017-01-12 20:25:01 +0100

    Added calibration and phase lock checks to synthHD

--- ecpy_hqc_legacy/instruments/drivers/visa/windfreaktech_synthHD_signal_generator.py
+++ ecpy_hqc_legacy/instruments/drivers/visa/windfreaktech_synthHD_signal_generator.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # -----------------------------------------------------------------------------
-# Copyright 2015-2016 by EcpyHqcLegacy Authors, see AUTHORS for more details.
+# Copyright 2015-2017 by EcpyHqcLegacy Authors, see AUTHORS for more details.
 #
 # Distributed under the terms of the BSD license.
 #
@@ -53,29 +53,57 @@ class SynthHD(VisaInstrument):
     output : bool, instrument_property
         State of the output 'ON'(True)/'OFF'(False).
     """
-    
+
     def __init__(self, connection_info, caching_allowed=True,
                  caching_permissions={}, auto_open=True):
 
         super(SynthHD, self).__init__(connection_info, caching_allowed,
-                                         caching_permissions, auto_open)
-        self.channel = 0
+                                      caching_permissions, auto_open)
         self.frequency_unit = 'GHz'
         self.write_termination = ''
         self.read_termination = ''
 
     @instrument_property
     @secure_communication()
+    def channel(self):
+        """Frequency getter method
+        """
+        self.write('C?')
+        output = self.read()
+        if output:
+            channel = output[0]
+            return channel
+        else:
+            mes = 'Instrument did not return its channel'
+            raise InstrIOError(mes)
+
+    @channel.setter
+    @secure_communication()
+    def channel(self, value):
+        """Frequency setter method
+        """
+        self.write('C'+format(value))
+        self.write('C?')
+        output = self.read()
+        if output:
+            if int(output) != int(value):
+                mes = 'Instrument did not set the channel correctly'
+                raise InstrIOError(mes)
+        else:
+            mes = 'Instrument did not return its channel'
+            raise InstrIOError(mes)
+
+    @instrument_property
+    @secure_communication()
     def frequency(self):
         """Frequency getter method
         """
-        self.write('C'+str(self.channel))
         self.write('f?')
         freq = self.read()
         if freq:
             return freq
-            mes = 'Instrument did not return the frequency'
         else:
+            mes = 'Instrument did not return the frequency'
             raise InstrIOError(mes)
 
     @frequency.setter
@@ -84,23 +112,21 @@ class SynthHD(VisaInstrument):
         """Frequency setter method
         """
         unit = self.frequency_unit
-        channel = self.channel
         valueMHz = value*CONVERSION_FACTORS[unit]['MHz']
         valueMHz_format = '{:.4f}'.format(valueMHz)
-        self.write('C'+str(channel))
         self.write('f'+str(valueMHz_format))
-        self.write('f?') # asks for frequency of current channel
-        result = float(self.read()) # returns frequency in MHz
+        self.write('f?')  # asks for frequency of current channel
+        result = float(self.read())  # returns frequency in MHz
         if abs(result - valueMHz) > 10**-12:
             mes = 'Instrument did not set correctly the frequency'
             raise InstrIOError(mes)
+        self.check_calibration()
 
     @instrument_property
     @secure_communication()
     def power(self):
         """Power getter method
         """
-        self.write('C'+str(self.channel))
         self.write('W?')
         power = self.read()
         if power is not None:
@@ -114,20 +140,18 @@ class SynthHD(VisaInstrument):
     def power(self, value):
         """Power setter method
         """
-        self.write('C'+str(self.channel))
         self.write('W'+str('{:.4f}'.format(value)))
         self.write('W?')
         result = float(self.read())
         if abs(result - value) > 10**-12:
             raise InstrIOError('Instrument did not set correctly the power')
+        self.check_calibration()
 
     @instrument_property
     @secure_communication()
     def output(self):
         """Output getter method
         """
-        channel = self.channel
-        self.write('C'+str(channel))
         self.write('E?r?')
         outputE = self.read()
         outputr = self.read()
@@ -140,7 +164,8 @@ class SynthHD(VisaInstrument):
                 return False
             else:
                 mes = '''The synthHD is in an intermediate state,
-                neither fully quiet, nor fully operational '''
+                neither fully quiet, nor fully operational,
+                received {} {}'''.format(outputE, outputr)
                 raise InstrIOError(mes)
         else:
             mes = 'SynthHD signal generator did not return its output'
@@ -151,8 +176,6 @@ class SynthHD(VisaInstrument):
     def output(self, value):
         """Output setter method
         """
-        channel = self.channel
-        self.write('C'+str(channel))
         on = re.compile('on', re.IGNORECASE)
         off = re.compile('off', re.IGNORECASE)
         if on.match(value) or value == 1:
@@ -169,3 +192,17 @@ class SynthHD(VisaInstrument):
             mess = fill(cleandoc('''The invalid value {} was sent to
                         switch_on_off method''').format(value), 80)
             raise VisaTypeError(mess)
+        self.check_calibration()
+
+    def check_calibration(self):
+        self.write('V')
+        output = self.read()
+        if not int(output[0]):
+            mes = 'SynthHD did not calibrate freq or power properly'
+            raise InstrIOError(mes)
+        if self.output:
+            self.write('p')
+            output = self.read()
+            if not int(output[0]):
+                mes = 'SynthHD did not phase lock'
+                raise InstrIOError(mes)

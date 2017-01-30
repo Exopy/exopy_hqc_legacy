8d952e4fc673c052087cc6734712f9f9039d42b0
leghtas <leghtas@gmail.com>
2017-01-30 22:27:00 +0100

    instr: alazar

--- ecpy_hqc_legacy/tasks/tasks/instr/spdev_tasks.py
+++ ecpy_hqc_legacy/tasks/tasks/instr/spdev_tasks.py
@@ -13,12 +13,10 @@ from __future__ import (division, unicode_literals, print_function,
                         absolute_import)
 
 import numbers
-
 import numpy as np
 from atom.api import (Bool, Unicode, set_default)
 
 from ecpy.tasks.api import InstrumentTask, validators
-
 
 VAL_REAL = validators.Feval(types=numbers.Real)
 
@@ -29,6 +27,10 @@ class DemodSPTask(InstrumentTask):
     """Get the averaged quadratures of the signal.
 
     """
+
+    # return averaged data or single shot data
+    average = Bool(True).tag(pref=True)
+
     #: Should the acquisition on channel 1 be enabled
     ch1_enabled = Bool(True).tag(pref=True)
 
@@ -42,10 +44,10 @@ class DemodSPTask(InstrumentTask):
     ch2_trace = Bool(False).tag(pref=True)
 
     #: Frequency of the signal sent to channel 1 in MHz
-    freq_1 = Unicode('20').tag(pref=True, feval=VAL_REAL)
+    freq_1 = Unicode('50').tag(pref=True, feval=VAL_REAL)
 
     #: Frequency of the signal sent to channel 2 in MHz
-    freq_2 = Unicode('20').tag(pref=True, feval=VAL_REAL)
+    freq_2 = Unicode('50').tag(pref=True, feval=VAL_REAL)
 
     #: Time during which to acquire data after a trigger (ns).
     duration = Unicode('0').tag(pref=True, feval=VAL_REAL)
@@ -57,7 +59,8 @@ class DemodSPTask(InstrumentTask):
     records_number = Unicode('1000').tag(pref=True, feval=VAL_INT)
 
     database_entries = set_default({'Ch1_I': 1.0, 'Ch1_Q': 1.0,
-                                    'Ch2_I': 1.0, 'Ch2_Q': 1.0})
+                                    'Ch2_I': 1.0, 'Ch2_Q': 1.0,
+                                    'Chc_I': 1.0, 'Chc_Q': 1.0})
 
     def check(self, *args, **kwargs):
         """Check that parameters make sense.
@@ -98,7 +101,6 @@ class DemodSPTask(InstrumentTask):
         """
         if self.driver.owner != self.name:
             self.driver.owner = self.name
-
             self.driver.configure_board()
 
         records_number = self.format_and_eval_string(self.records_number)
@@ -106,39 +108,98 @@ class DemodSPTask(InstrumentTask):
         duration = self.format_and_eval_string(self.duration)*1e-9
 
         channels = (self.ch1_enabled, self.ch2_enabled)
+
         ch1, ch2 = self.driver.get_traces(channels, duration, delay,
                                           records_number)
+
         if self.ch1_enabled:
             f1 = self.format_and_eval_string(self.freq_1)*1e6
-            phi1 = np.linspace(0, 2*np.pi*f1*duration, len(ch1))
+            ntraces1, nsamples1 = np.shape(ch1)
+            phi1 = np.linspace(0, 2*np.pi*f1*((nsamples1-1)*2e-9), nsamples1)
             c1 = np.cos(phi1)
             s1 = np.sin(phi1)
             # The mean value of cos^2 is 0.5 hence the factor 2 to get the
             # amplitude.
-            self.write_in_database('Ch1_I', 2*np.mean(ch1*c1))
-            self.write_in_database('Ch1_Q', 2*np.mean(ch1*s1))
+            Ch1_I = 2*np.mean(ch1*c1, axis=1)
+            Ch1_Q = 2*np.mean(ch1*s1, axis=1)
+            Ch1_I_av = Ch1_I if not self.average else np.mean(Ch1_I)
+            Ch1_Q_av = Ch1_Q if not self.average else np.mean(Ch1_Q)
+            self.write_in_database('Ch1_I', Ch1_I_av)
+            self.write_in_database('Ch1_Q', Ch1_Q_av)
+
             if self.ch1_trace:
-                self.write_in_database('Ch1_trace', ch1)
+                ch1_av = ch1 if not self.average else np.mean(ch1, axis=0)
+                self.write_in_database('Ch1_trace', ch1_av)
 
         if self.ch2_enabled:
             f2 = self.format_and_eval_string(self.freq_2)*1e6
-            phi2 = np.linspace(0, 2*np.pi*f2*duration, len(ch2))
+            ntraces2, nsamples2 = np.shape(ch2)
+            phi2 = np.linspace(0, 2*np.pi*f2*((nsamples2-1)*2e-9), nsamples2)
             c2 = np.cos(phi2)
             s2 = np.sin(phi2)
             # The mean value of cos^2 is 0.5 hence the factor 2 to get the
             # amplitude.
-            self.write_in_database('Ch2_I', 2*np.mean(ch2*c2))
-            self.write_in_database('Ch2_Q', 2*np.mean(ch2*s2))
+            Ch2_I = 2*np.mean(ch2*c2, axis=1)
+            Ch2_Q = 2*np.mean(ch2*s2, axis=1)
+            Ch2_I_av = Ch2_I if not self.average else np.mean(Ch2_I)
+            Ch2_Q_av = Ch2_Q if not self.average else np.mean(Ch2_Q)
+            self.write_in_database('Ch2_I', Ch2_I_av)
+            self.write_in_database('Ch2_Q', Ch2_Q_av)
+
             if self.ch2_trace:
-                self.write_in_database('Ch2_trace', ch2)
+                ch2_av = ch2 if not self.average else np.mean(ch2, axis=0)
+                self.write_in_database('Ch2_trace', ch2_av)
+
+        if self.ch1_enabled and self.ch2_enabled:
+            Ch1_c = Ch1_I + 1j*Ch1_Q
+            Ch2_c = Ch2_I + 1j*Ch2_Q
+            Chc_I = np.real(Ch1_c/Ch2_c)
+            Chc_Q = np.imag(Ch1_c/Ch2_c)
+            Chc_I_av = Chc_I if not self.average else np.mean(Chc_I)
+            Chc_Q_av = Chc_Q if not self.average else np.mean(Chc_Q)
+            self.write_in_database('Chc_I', Chc_I_av)
+            self.write_in_database('Chc_Q', Chc_Q_av)
+            if self.ch1_trace:
+                samples_per_period = int(500e6/f1)
+                samples_per_trace = len(ch1[0])
+                Ch1_c1 = ch1*c1
+                Ch1_s1 = ch1*s1
+                if (samples_per_trace % samples_per_period) != 0:
+                    Ch1_c1 = Ch1_c1[:,
+                                    :-(samples_per_trace % samples_per_period)]
+                    Ch1_s1 = Ch1_s1[:,
+                                    :-(samples_per_trace % samples_per_period)]
+                Ch1_c1 = Ch1_c1.reshape((records_number,
+                                         samples_per_trace//samples_per_period,
+                                         samples_per_period))
+                Ch1_I_t = 2*np.mean(Ch1_c1, axis=2)
+                Ch1_s1 = Ch1_s1.reshape((records_number,
+                                         samples_per_trace//samples_per_period,
+                                         samples_per_period))
+                Ch1_Q_t = 2*np.mean(Ch1_s1, axis=2)
+
+                Ch1_c_t = Ch1_I_t + 1j*Ch1_Q_t
+                Ch1_c_corr = np.transpose(np.transpose(Ch1_c_t)/Ch2_c)
+                if not self.average:
+                    Ch1_c_corr_av = Ch1_c_corr
+                else:
+                    Ch1_c_corr_av = np.mean(Ch1_c_corr, axis=0)
+                ChI_t_corr = np.real(Ch1_c_corr_av)
+                ChQ_t_corr = np.imag(Ch1_c_corr_av)
+                self.write_in_database('ChI_trace_corr', ChI_t_corr)
+                self.write_in_database('ChQ_trace_corr', ChQ_t_corr)
 
     def _post_setattr_ch1_enabled(self, old, new):
         """Update the database entries based on the enabled channels.
 
         """
-        entries = {'Ch1_I': 1.0, 'Ch1_Q': 1.0}
+        # TODO: how do we add the corrected I, Q Chc_I and Chc_Q
+        # to the database correctly?
+        entries = {'Ch1_I': 1.0, 'Ch1_Q': 1.0, 'Chc_I': 1.0, 'Chc_Q': 1.0}
         if self.ch1_trace:
             entries['Ch1_trace'] = np.array([0, 1])
+            entries['ChI_trace_corr'] = np.array([0, 1])
+            entries['ChQ_trace_corr'] = np.array([0, 1])
         self._update_entries(new, entries)
 
     def _post_setattr_ch2_enabled(self, old, new):
@@ -156,7 +217,9 @@ class DemodSPTask(InstrumentTask):
         """
         if new and not self.ch1_enabled:
             return
-        self._update_entries(new, {'Ch1_trace': np.array([0, 1])})
+        self._update_entries(new, {'Ch1_trace': np.array([0, 1]),
+                                   'ChI_trace_corr': np.array([0, 1]),
+                                   'ChQ_trace_corr': np.array([0, 1])})
 
     def _post_setattr_ch2_trace(self, old, new):
         """Update the database entries based on the trace settings.

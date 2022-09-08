61575957f82ddcab8e9f5e4d23eeae6046fb0b42
William Legrand <william.legrand@mat.ethz.ch>
2022-09-08 15:16:37 +0200

    Updated sine fits to also have robust, fixed phase, amplitude fitting function

--- exopy_hqc_legacy/tasks/tasks/util/fit_sine_tasks.py
+++ exopy_hqc_legacy/tasks/tasks/util/fit_sine_tasks.py
@@ -26,7 +26,8 @@ ARR_VAL = validators.Feval(types=np.ndarray)
 
 
 class FitSineTask(SimpleTask):
-    """ Fit an array frm the database to a sine.
+    """ Fit an array from the database to a cosine
+        at a given freq.
 
     """
     #: Name of the targets in the database.
@@ -38,18 +39,22 @@ class FitSineTask(SimpleTask):
                     pref=True, 
                     feval=validators.SkipLoop(types=numbers.Real)
                     )
-
+    tol_freq = Str('').tag(
+                    pref=True, 
+                    feval=validators.SkipLoop(types=numbers.Real)
+                    )
 
     wait = set_default({'activated': True})  # Wait on all pools by default.
 
     database_entries = set_default({'freq_value': 1.0 , 'ampl_value': 1.0, 'phase_value': 1.0,
-                                    'offset_value':1.0, 'res_value': 1.0, 'fit_err': 0.0,
+                                    'offset_value': 1.0, 'res_value': 1.0, 'fit_err': 0.0,
                                     'fitted_array': np.array([1.0])})
 
     def perform(self):
         t = self.format_and_eval_string(self.time_array_in)
         v = self.format_and_eval_string(self.val_array_in)
         freq = self.format_and_eval_string(self.freq)
+        tol_freq = self.format_and_eval_string(self.tol_freq)
 
         guess_ampl = find_ampl(t,v,freq)
         guess_phase = find_phasepoint(t,v,freq)
@@ -58,7 +63,7 @@ class FitSineTask(SimpleTask):
             log = logging.getLogger(__name__)
             msg = ('Fit starts with {}, {}')
             log.warning(msg.format(guess_ampl, guess_phase))
-            popt, fit_err = fit_mod(t,v,freq,guess_ampl,guess_phase)
+            popt, fit_err = fit_mod(t,v,freq,tol_freq,guess_ampl,guess_phase)
         except:
             popt = [1e9,1e9,1e9,1e9]
             fit_err = 100
@@ -77,6 +82,63 @@ class FitSineTask(SimpleTask):
                    'phase fit = {} rad, relative error = {}')
             log.warning(msg.format(round(popt[0], 3), round(fit_err, 2)))
 
+class FitCosAndSinTask(SimpleTask):
+    """ Fit an array from the database to a cos and sine
+        at a given freq and phase.
+
+    """
+    #: Name of the targets in the database.
+    time_array_in = Str().tag(pref=True, feval=ARR_VAL)
+    val_array_in = Str().tag(pref=True, feval=ARR_VAL)
+
+    #: Given parameters.
+    freq = Str('').tag(
+                    pref=True, 
+                    feval=validators.SkipLoop(types=numbers.Real)
+                    )
+    tol_freq = Str('').tag(
+                    pref=True, 
+                    feval=validators.SkipLoop(types=numbers.Real)
+                    )
+    phase = Str('').tag(
+                    pref=True, 
+                    feval=validators.SkipLoop(types=numbers.Real)
+                    )
+
+    wait = set_default({'activated': True})  # Wait on all pools by default.
+
+    database_entries = set_default({'ampl_cos_value': 1.0,
+                                    'ampl_sin_value': 1.0,
+                                    'freq_value': 1.0,
+                                    'offset_value': 1.0,
+                                    'res_value': 1.0,
+                                    'fit_err': 0.0,
+                                    'fitted_array': np.array([1.0])})
+
+    def perform(self):
+        t = self.format_and_eval_string(self.time_array_in)
+        v = self.format_and_eval_string(self.val_array_in)
+        freq = self.format_and_eval_string(self.freq)
+        tol_freq = self.format_and_eval_string(self.tol_freq)
+        phase = self.format_and_eval_string(self.phase)
+
+        guess_ampl = find_ampl(t,v,freq)
+        popt, fit_err = fit_knownphase(t,v,freq,tol_freq,phase,guess_ampl)
+
+        try:
+            popt, fit_err = fit_knownphase(t,v,freq,tol_freq,phase,guess_ampl)
+        except:
+            popt = [1e9,1e9,1e9,1e9,1e9]
+            fit_err = 100
+
+        self.write_in_database('res_value', np.sqrt((v-cossin_funct(t, phase, *popt))**2))
+        self.write_in_database('fit_err', fit_err)
+        self.write_in_database('freq_value', popt[0])
+        self.write_in_database('ampl_cos_value', popt[1])
+        self.write_in_database('ampl_sin_value', popt[2])
+        self.write_in_database('offset_value', popt[3])
+        self.write_in_database('fitted_array', cossin_funct(t, phase, *popt))
+
 
 def find_phasepoint(t,v,freq):
     '''
@@ -94,7 +156,7 @@ def find_phasepoint(t,v,freq):
     period_idx=np.searchsorted(t-t[0],1/(freq))
     max_idx=np.argmax(v[:period_idx+1])
     
-    return np.mod(2*np.pi*t[0]*freq+2*np.pi*max_idx/period_idx+np.pi,2*np.pi)-np.pi
+    return np.mod(-2*np.pi*t[max_idx]*freq+np.pi,2*np.pi)-np.pi
 
 
 def find_ampl(t,v,freq):
@@ -111,20 +173,33 @@ def find_ampl(t,v,freq):
 
     '''
     
-    return np.sqrt(2)*np.mean(np.sqrt(v**2))
+    return np.sqrt(2*np.mean((v-np.mean(v))**2))
 
 
 def mod_funct(t, ph, freq, amp, off):
-    return amp*np.cos(2*np.pi*freq*t-ph)+off
+    return amp*np.cos(2*np.pi*freq*t+ph)+off
 
+def cossin_funct(t, ph, freq, amp_cos, amp_sin, off):
+    return amp_cos*np.cos(2*np.pi*freq*t+ph)+amp_sin*np.sin(2*np.pi*freq*t+ph)+off
 
-def fit_mod(t,v,freq,guess_ampl,guess_phase):
+def fit_mod(t,v,freq,tol_freq,guess_ampl,guess_phase):
 
-    popt, pcov = curve_fit(mod_funct, t, v, (guess_phase, freq, guess_ampl, 0))
-    if popt[2]<0:
-        popt[2]=-popt[2]
-        popt[0]=popt[0]+np.pi
+    guess_off=np.mean(v)
+    popt, pcov = curve_fit(mod_funct, t, v, p0=[guess_phase, freq, guess_ampl, guess_off], 
+        bounds=([-np.inf, freq-tol_freq, 0, -np.inf],[np.inf, freq+tol_freq, np.inf, np.inf])
+        )
     popt[0]=np.mod(popt[0]+np.pi,2*np.pi)-np.pi
     fit_error = 100*np.sqrt(pcov[0, 0])/(2*np.pi)
+
+    return popt, fit_error
+
+def fit_knownphase(t,v,freq,tol_freq,phase,guess_ampl):
+
+    guess_off=np.mean(v)
+    popt, pcov = curve_fit(lambda x, popt0, popt1, popt2, popt3: cossin_funct(x, phase, popt0, popt1, popt2, popt3),
+        t, v, p0=[freq, guess_ampl/np.sqrt(2), guess_ampl/np.sqrt(2), guess_off], 
+        bounds=([freq-tol_freq, -np.inf, -np.inf, -np.inf],[freq+tol_freq, np.inf, np.inf, np.inf])
+        )
+    fit_error = 50*np.sqrt(pcov[2, 2])+50*np.sqrt(pcov[3, 3])
 
     return popt, fit_error
